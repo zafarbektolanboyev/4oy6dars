9cf25c1c96316e85da17d7f551753b9025ba00af
zafarbek tolanboyev <zafarvektolanboyev618@gmail.com>
2024-02-05 09:45:57 +0500

    indexhtmlappjs

---
+++ app.js
@@ -0,0 +1,44 @@
+// let n = 5;
+// let counter = 0;
+
+// for(let i = 0; i <= n; i++){
+//     console.log(`(${n} + ${i}) ** 2 = ${(n + i) ** 2}`);
+//     counter += (n + i) ** 2;
+// }
+// console.log("Sum ", counter);
+
+
+// let n = 5;
+// let pover = n;
+// let sum = 0;
+
+// for(let i = 1; i <= n; i++){
+//     sum += i ** pover;
+//     pover--;
+// }
+// console.log(sum)
+
+
+
+// let sum = "";
+// for(let i = 1; i <= 5; i++){
+//     for(let k = 1; k <= i; k++){
+//         sum += `${i} `;
+//     }
+//     console.log(sum.trimEnd());
+//     sum = "";
+// }
+
+
+// let n = 10;
+// let counter = "";
+
+// for(let i = 1; i <= n; i++){
+//     if(n % i == 0){
+//         counter += `${i} `;
+//     }
+// }
+// console.log(counter.trimEnd());
+
+
+

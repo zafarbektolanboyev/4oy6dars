ff5a5e40af2a6b1f8677f1c5c139ee53c0c2d015
zafarbek tolanboyev <zafarvektolanboyev618@gmail.com>
2024-02-06 17:18:28 +0500

    appjsindexhtml

--- app.js
+++ app.js
@@ -41,4 +41,39 @@
 // console.log(counter.trimEnd());
 
 
+// Array.isArray(arr) - ma'lumot arrayligini.(true,false);
+// 
+// const names = [
+//     "Otabek",
+//     "Zafarbek",
+//     "Samandar",
+//     "Ikromxo'ja",
+//     "Fayzulloxon",
+//     "Muhammadzoir",
+//     "Jasur",
+//     "Sunatilo",
+//     "Izzatilo",
+//     "Asror",
+//     "Asadbek",
+// ];
+// // console.log(names[names.length-1])
+// console.log(names.at(-1))
 
+
+
+// array methods
+// Array length ----- array ichidagi elementlar sonini chiqarib beradi
+// Array shift() ----- massivning birinchi elemntini olib tashladi va qolgan elemntlarni indexga saqlaydi
+// Array toString() ------ array (vergul bilan ajratilgan)array qiymatlari qatoriga aylantiradi
+// Array unshift() ----- Usul unshift()massivga yangi element qo'shadi (boshida) va eski elementlarni "o'chiradi"
+// Array at()------ at yordamida elemenlarni raqamini yozib chaqring	
+// Array delete()
+// Array join()------Usul join(), shuningdek, barcha massiv elementlarini satrga birlashtiradi
+// Array contact()-----
+// Usul concat()mavjud massivlarni birlashtirish (birlashtirish) orqali yangi massiv yaratadi
+// Array pop()------Usul pop()massivdan oxirgi elementni olib tashlaydi	
+// Array copy Within()------Usul copyWithin()massiv elementlarini massivdagi boshqa joyga ko'chiradi:
+// Array push()-----Usul push()massivga yangi element qo'shadi (oxirida):	
+// Array flat()----Usul flat()ma'lum bir chuqurlikka birlashtirilgan pastki qator elementlari bilan yangi massiv yaratadi.
+// Array splice()-----Usul splice()massivga yangi elementlar qo'shish uchun ishlatilishi mumkin	
+// Array slice()------Usul slice()massivning bir qismini yangi massivga ajratadi:
